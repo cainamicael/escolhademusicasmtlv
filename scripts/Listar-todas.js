307532b712fd86f048f488cf6edac31da608c3d8
Cainã Micael™ <83286162+cainamicael@users.noreply.github.com>
2023-09-07 17:44:54 -0300

    atualizacao

--- scripts/Listar-todas.js
+++ scripts/Listar-todas.js
@@ -2,43 +2,42 @@ alert('Quando todas as músicas forem tocadas, a contagem de músicas tocadas ir
 
 //Setando as músicas cadastradas e tocadas
 fetch(urlBase + 'quantidades')
-.then(resposta => resposta.json())
-.then(quantidades => {
-    const musicasCadastradas = quantidades.quantidadeRegistros
-    const musicasTocadas = quantidades.quantidadeMusicasTocadas
+    .then(resposta => resposta.json())
+    .then(quantidades => {
+        const musicasCadastradas = quantidades.quantidadeRegistros
+        const musicasTocadas = quantidades.quantidadeMusicasTocadas
 
-    //Mudando os textos
-    document.getElementById('musicas-cadastradas').innerText = musicasCadastradas + ' MÚSICAS CADASTRADAS'
-    
-    document.getElementById('musicas-tocadas').innerText = musicasTocadas + ' MÚSICAS JÁ TOCADAS'
-})
+        //Mudando os textos
+        document.getElementById('musicas-cadastradas').innerText = musicasCadastradas + ' MÚSICAS CADASTRADAS'
+
+        document.getElementById('musicas-tocadas').innerText = musicasTocadas + ' MÚSICAS JÁ TOCADAS'
+    })
 
 //Pegando cada música dinamicamente
 fetch(urlBase + 'musicas')
-.then(resposta => resposta.json())
-.then(dados => {
-    const modulos = document.querySelector('.modulos')
-    modulos.innerHTML = ''
-    let adicionar = ''
+    .then(resposta => resposta.json())
+    .then(dados => {
+        const modulos = document.querySelector('.modulos')
+        modulos.innerHTML = ''
+        let adicionar = ''
 
-    if(dados != []) {
-        dados.forEach(dado => {  
-            console.log(dado)
-            let dataFormatada = ''
+        if (dados != []) {
+            dados.forEach(dado => {
+                let dataFormatada = ''
 
-            if(dado.dataUltimaVezTocada != null) {
-                const data = new Date(dado.dataUltimaVezTocada);
+                if (dado.dataUltimaVezTocada != null) {
+                    const data = new Date(dado.dataUltimaVezTocada);
 
-                const dia = data.getDate();
-                const mes = data.getMonth() + 1; // Lembrando que os meses em JavaScript são base 0
-                const ano = data.getFullYear();
-    
-                dataFormatada = `${dia}/${mes}/${ano}`
-            } else {
-                dataFormatada = 'Esta música ainda não foi tocada'
-            }
+                    const dia = data.getDate();
+                    const mes = data.getMonth() + 1; // Lembrando que os meses em JavaScript são base 0
+                    const ano = data.getFullYear();
 
-            adicionar += `
+                    dataFormatada = `${dia}/${mes}/${ano}`
+                } else {
+                    dataFormatada = 'Esta música ainda não foi tocada'
+                }
+
+                adicionar += `
                 <div class="modulo">
                     <span>ID:</span>
                     <p>${dado.id}</p>
@@ -54,10 +53,101 @@ fetch(urlBase + 'musicas')
     
                     <span>QUANDO FOI TOCADA: </span>
                     <p>${dataFormatada}</p>
+
+                    <button class="editar" id="botao-editar" onclick="abrirModal(${dado.id})">Editar ✎</button>
                 </div>
             `
-        })
-    }
+            })
+        }
 
-    modulos.innerHTML = adicionar
-})
+        modulos.innerHTML = adicionar
+    })
+
+function abrirModal(id) {
+    fetch(urlBase + `musica/${id}`)
+        .then(res => res.json())
+        .then(dado => {
+            const modal = document.querySelector('.modal')
+            modal.innerHTML = `
+
+            <div class="modal-header">
+                <span>Editar - Faça as Alterações</span>
+                <button class="fechar-modal" onclick="fecharModal()">Fechar</button>
+            </div>
+
+            <div class="modal-main">
+                <form>
+                    <input type="text" name="nome" placeholder="Nome da música..." value="${dado.nome}" required>
+                    <input type="text" name="cantor" placeholder="Nome de quem canta..." value="${dado.cantor}"
+                        required>
+
+                    <label for="selecao">Categoria: </label>
+                    <select name="categoria" id="selecao">
+                        <option value="">Selecione</option>
+                        <option value="ADORACAO">Adoração</option>
+                        <option value="APELO">Apelo</option>
+                        <option value="CEIA">Ceia</option>
+                        <option value="CELEBRACAO">Celebração</option>
+                        <option value="CIRCULO_DE_ORACAO">Círculo de Oração</option>
+                        <option value="CORINHOS">Corinhos</option>
+                        <option value="FAMILIA">Família</option>
+                        <option value="GRATIDAO">Gratidão</option>
+                        <option value="MISSOES">Missões</option>
+                        <option value="MULHERES">Mulheres</option>
+                        <option value="NATAL">Natal</option>
+                    </select>
+                </form>
+            </div>
+
+            <div class="modal-footer">
+                <button class="salvar-modal" onclick="salvarModal(${dado.id})">Salvar</button>
+            </div>
+            `
+
+            const fade = document.querySelector('.fade')
+            modal.classList.toggle('hide')
+            fade.classList.toggle('hide')
+        })
+        .catch(e => alert(`Ocorreu um erro no servidor! ${e}`))
+}
+
+function fecharModal() {
+    const modal = document.querySelector('.modal')
+    const fade = document.querySelector('.fade')
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+    modal.innerHTML = ''
+}
+
+async function salvarModal(id) {
+    //verificar se a categoria está selecionada
+    if(document.getElementsByName('categoria')[0].value != '') {
+        //Pegar os dados e enviar via post
+        const nome = document.getElementsByName('nome')[0].value
+        const cantor = document.getElementsByName('cantor')[0].value
+        const categoria = document.getElementsByName('categoria')[0].value
+
+        const obj = { nome, cantor, categoria }
+
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        }
+
+        const res = await fetch(urlBase + `musica/${id}`, config)
+        if (res.ok) {
+            alert('Música Atualizada!')
+            fecharModal()
+
+            //recarregar a página para já mostrar os valores atualizados
+            location.reload()
+        } else {
+            alert('Erro ao tentar editar música')
+        }
+    } else {
+        alert('Escolha a categoria para salvar!')
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,101 +1,166 @@
 alert('AVISO: Não esqueça de clicar no confirmar e caso desista, clique em cancelar!')
 
+//Remover a classe inicio
+function removerInicio() {
+    document.querySelectorAll('.inicio').forEach(inicio => inicio.remove())
+}
 
-//Pegar a quantidade de músicas
-var valorSelecionado = 3
+//Onchange do select
+function pegarValor() {
+    const valor = document.getElementById('selecao').value
+    if (valor != '') {
 
-const modulos = document.querySelector('.modulos')
-modulos.innerHTML = ''
+        //tirar a classe inicio
+        removerInicio()
 
-iterarQuantidades(valorSelecionado)
-.then(resultado => modulos.innerHTML = resultado)
-
-async function iterarQuantidades(valorSelecionado) {
-    var resultados = []
-
-    for (let i = 0; i < valorSelecionado; i++) {
-        const categoria = (i === valorSelecionado - 1) ? 'celebracao' : 'adoracao'
-        const res = await fetch(urlBase + `musica?categoria=${categoria}`)
-        const musica = await res.json()
-
-        resultados.push(`
-            <div class="modulo" id="modulo-${i + 1}">
-                <h4>${i + 1}ª</h4>
-                <input type="hidden" id="hidden-${i + 1}" value="${musica.id}">
-                <h2 class="musica" id="musica-${i + 1}">${musica.nome}</h2>
-                <h2 class="cantor" id="cantor-${i + 1}">(${musica.cantor})</h2>
-                <button id="botao-${i + 1}" onclick="botaoClicado(${musica.id}, ${i + 1})">Escolher outra Música</button>
-            </div>
-        `)
+        //adicionar módulo com a categoria selecionada
+        fetch(urlBase + `musica?categoria=${valor}`)
+            .then(res => res.json())
+            .then(musica => {
+                const modulos = document.querySelector('.modulos').innerHTML
+                document.querySelector('.modulos').innerHTML = modulos + `
+                <div class="modulo" id="modulo-${musica.id}">
+                    <h4>♬</h4>
+                    <input type="hidden" id="id" value="${musica.id}">
+                    <input type="hidden" id="categoria-${musica.id}" value="${valor}">
+                    <h2 class="musica" id="musica-${musica.id}">${musica.nome}</h2>
+                    <h2 class="cantor" id="cantor-${musica.id}">(${musica.cantor})</h2>
+                    <button id="botao-${musica.id}" onclick="botaoClicado(${musica.id})">Escolher outra Música</button>
+                    <button id="adicionar" onclick="adicionar()">+</button>
+                    <button id="remover" onclick="remover(${musica.id})">-</button>
+                </div>
+            `
+            })
     }
-    return resultados.join('');
+}
+
+//Quando clicamos no botão +
+function adicionar() {
+    //Pegar tudo o que tem nos módulos, guardar e concatenar
+    const modulos = document.querySelector('.modulos').innerHTML
+
+    document.querySelector('.modulos').innerHTML = modulos + `
+        <div class="inicio">
+            <h1>Selecione a catagoria da música:</h1>
+
+            <select name="categoria" id="selecao" onchange="pegarValor()">
+                <option value="">Selecione</option>
+                <option value="ADORACAO">Adoração</option>
+                <option value="APELO">Apelo</option>
+                <option value="CEIA">Ceia</option>
+                <option value="CELEBRACAO">Celebração</option>
+                <option value="CIRCULO_DE_ORACAO">Círculo de Oração</option>
+                <option value="CORINHOS">Corinhos</option>
+                <option value="FAMILIA">Família</option>
+                <option value="GRATIDAO">Gratidão</option>
+                <option value="MISSOES">Missões</option>
+                <option value="MULHERES">Mulheres</option>
+                <option value="NATAL">Natal</option>
+            </select>
+        </div>
+    `
+
+    let totalInicios = document.querySelectorAll('.inicio').length
+
+    document.querySelectorAll('.inicio').forEach((e, indice) => {
+        if (indice != totalInicios - 1) {
+            e.remove()
+        }
+    })
+}
+
+//Quando clicamos no botão -
+function remover(id) {
+    const totalElementos = document.querySelectorAll(`.modulo`).length
+
+    if (totalElementos > 1) {
+        const removerElemento = document.querySelector(`#modulo-${id}`).remove()
+    } else {
+        alert('Insira outras músicas para poder remover esta, ou clique em cancelar e começe do zero')
+    }
+
 }
 
 //Escolhendo outra música
-function botaoClicado(id, posicao) {
+function botaoClicado(id) {
     fetch(urlBase + `musica/pular/${id}`)
-    .then(res => res.json())
-    .then(novaMusica => {
-        //Mudar os valores do módulo específico
-        const moduloEspecifico = document.getElementById(`modulo-${posicao}`)
+        .then(res => res.json())
+        .then(musica => {
+            //Mudar os valores do módulo específico
+            const moduloEspecifico = document.getElementById(`modulo-${id}`)
 
-        moduloEspecifico.innerHTML = `
-            <h4>${posicao}ª</h4>
-            <input type="hidden" id="hidden-${posicao}" value="${novaMusica.id}">
-            <h2 class="musica" id="musica-${posicao}">${novaMusica.nome}</h2>
-            <h2 class="cantor" id="cantor-${posicao}">(${novaMusica.cantor})</h2>
-            <button id="botao-${posicao}" onclick="botaoClicado(${novaMusica.id}, ${posicao})">Escolher outra Música</button>
+            moduloEspecifico.innerHTML = `
+            <h4>♬</h4>
+            <input type="hidden" id="id" value="${musica.id}">
+            <input type="hidden" id="categoria-${musica.id}" value="${musica.categoria}">
+            <h2 class="musica" id="musica-${musica.id}">${musica.nome}</h2>
+            <h2 class="cantor" id="cantor-${musica.id}">(${musica.cantor})</h2>
+            <button id="botao-${musica.id}" onclick="botaoClicado(${musica.id})">Escolher outra Música</button>
+            <button id="adicionar" onclick="adicionar()">+</button>
+            <button id="remover" onclick="remover(${musica.id})">-</button>
         `
-    })
+            moduloEspecifico.id = `modulo-${musica.id}`
+        })
 }
 
 //Confirmando
 async function confirmar() {
-    const obj = []
+    //Para só poder fazer o post para confirmar as músicas se tiver alguma música selecionada
 
-    for(let i = 0; i < valorSelecionado; i++) {
-        const id = parseInt(document.getElementById(`hidden-${i + 1}`).value)
-        const nome = document.getElementById(`musica-${i + 1}`).textContent
-        const cantor = document.getElementById(`cantor-${i + 1}`).textContent.replace(/[()]/g, '')
-        const categoria = (i === valorSelecionado-1) ? 'CELEBRACAO' : 'ADORACAO'
+    if (document.querySelectorAll('.modulo').length > 0) {
+        const obj = []
 
-        obj.push({id, nome, cantor, categoria})
-    }
+        //Pegar todos os elementos com a classe .modulo e seus valores, e adicionar no array
+        const classeModulo = document.querySelectorAll('.modulo')
 
-    console.log(JSON.stringify(obj))
-    
-    const config = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(obj)
-    }
+        classeModulo.forEach(modulo => {
 
-    try {
-        const res = await fetch(urlBase + 'musicas/confirmar', config)
+            //Preciso do nome, cantor e categoria de cada div
+            const id = modulo.querySelector('#id').value
 
-        if(res.ok) { 
-            alert(`
-            Músicas confirmadas! 
-            
-            Se você estiver usando o celular, 
-            você pode copiar e colar no grupo:
+            const nome = modulo.querySelector(`#musica-${id}`).textContent
+            //Remove o primeiro e o último caractere ( )
+            const cantor = modulo.querySelector(`#cantor-${id}`).textContent.slice(1).slice(0, -1)
+            const categoria = modulo.querySelector(`#categoria-${id}`).value
 
-            Graça e paz a todos! 
-            Seguem as músicas que serão tocadas:
-            
-            ${obj[0].nome} - ${obj[0].cantor}
-            ${obj[1].nome} - ${obj[1].cantor}
-            ${obj[2].nome} - ${obj[2].cantor}
+            obj.push({id, nome, cantor, categoria })
+        })
 
-            Fiquem todos com Deus!
-        `)
-        } else {
-            alert('Música não confirmada! Ocorreu um erro!')
+        console.log(obj)
+
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
         }
-    }  catch(e) {
-        alert('Ouve um erro no servidor!')
+
+        try {
+            const res = await fetch(urlBase + 'musicas/confirmar', config)
+
+            if (res.ok) {
+                let adicionar = ''
+
+                obj.forEach(dado => {
+                    adicionar += `
+                        <h3>${dado.nome}</h3>
+                        <h4>(${dado.cantor})</h4>
+                        <br>
+                    `
+                })
+
+                document.querySelector('.musicas-modal').innerHTML = adicionar
+
+                abrirModal()
+            } else {
+                alert('Música não confirmada! Ocorreu um erro!')
+            }
+        } catch (e) {
+            alert('Ouve um erro no servidor!')
+        }
+    } else {
+        alert(`Escolha alguma música primeiro! `)
     }
 }
 
@@ -104,4 +169,19 @@ async function cancelar() {
     let res = await fetch(urlBase + 'musicas/cancelar')
     console.log(`Status da requisição de cancelamento: ${res.status}`)
     window.location.href = '../index.html'
+}
+
+function abrirModal() {
+    const modal = document.querySelector('.modal')
+    const fade = document.querySelector('.fade')
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+}
+
+function fecharModal() {
+    const modal = document.querySelector('.modal')
+    const fade = document.querySelector('.fade')
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+    document.querySelector('.musicas-modal').innerHTML = ``
 }
